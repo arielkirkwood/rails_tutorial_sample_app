b139f295272fa846468cb6de51899362a70aa05d
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-20 23:02:05 -0500

    Added support for has_secure_password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Ariel Kirkwood", email: "ariel.kirkwood@me.com")
+    @user = User.new(name: "Ariel Kirkwood", email: "ariel.kirkwood@me.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
