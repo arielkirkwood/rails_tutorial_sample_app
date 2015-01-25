3e1364ac3ff6f4b5c8aa756bc874e2082f48c9b6
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 16:48:39 -0500

    Adds tests for error markup on invalid submission

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
