17830a50e9b1345016b4fcf237d9ae6000551933
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 18:42:11 -0500

    (WIP) Logging in
    Controller code
    View code
    Tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
   root             'static_pages#home'
+
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+
   get 'signup'  => 'users#new'
   resources :users
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
