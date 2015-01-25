400c1338bf275c458db9f052b86e2b5e158f7f0e
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 11:42:09 -0500

    Mass assignment protection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
     else
       render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email,
+                                   :password, :password_confirmation)
+    end
 end
