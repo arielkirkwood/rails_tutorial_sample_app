ba8d797d175e7ae94dbb1825ba1eac90119afd6d
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 22:26:32 -0500

    SessionsHelper#current_user: Cookie support

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,15 @@
 module SessionsHelper
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def log_in(user)
