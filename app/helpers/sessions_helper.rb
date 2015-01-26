b5f18747a981a2cf17d6cea2747f49fca2a2caad
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 19:25:50 -0500

    SessionsHelper: adds logged_in? method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
