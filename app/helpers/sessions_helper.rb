c659a9732414fa2c948ff90836a6042e27a4742c
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 20:24:59 -0500

    Log out functionality
    Verified with additional assertions on UsersLoginTest.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,11 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
