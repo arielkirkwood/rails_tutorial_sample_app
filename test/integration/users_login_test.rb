03613401934edeffa0b431ffdf27795a8ed2e5d7
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-25 19:53:00 -0500

    Added user fixture & valid login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:ariel)
+  end
+
   test "user login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -9,5 +13,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     refute_empty flash
     get root_path
     assert_empty flash
+  end
+
+  test "user login with valid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
   end
 end
