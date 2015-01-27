7df0d0e44d91bc356c6bdcaa6bef09099743666b
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-27 18:26:45 -0500

    Edit/update actions, tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:ariel)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: { name: '',
+                                    email: 'foo@invalid',
+                                    password:              'foo',
+                                    password_confirmation: 'bar' }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'foo bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password:              '',
+                                    password_confirmation: '' }
+    refute_empty flash
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name,  name
+    assert_equal @user.email, email
+  end
+end
