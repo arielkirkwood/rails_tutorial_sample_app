bd4448a10a8098c0cc2088f2471c494ddd95bc27
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-27 19:22:48 -0500

    Typo (thanks, tests!)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def logged_in_user
