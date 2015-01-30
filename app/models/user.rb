08984f3fbfd6dd91cfa422d9346cfedada8f6c58
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-30 00:00:03 -0500

    Refactored code into User methods
    New methods:
    - User#activate
    - User#send_activation_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,11 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
@@ -37,6 +42,10 @@ class User < ActiveRecord::Base
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
