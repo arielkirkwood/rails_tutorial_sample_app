4ac1e32615c6a6787d7c00981fb6a9307f129d7d
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-02-02 15:51:32 -0500

    Moved #logged_in_user to ApplicationController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,14 +60,6 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = 'Please log in.'
-        redirect_to login_url
-      end
-    end
-
     def user_params
       params.require(:user).permit(:name, :email,
                                    :password, :password_confirmation)
