af6dd6f3634168f06a485f7ecd4f8b82db756c67
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-02-02 14:15:07 -0500

    Default scope for microposts is most recent first

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -24,4 +24,8 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.content = 'a' * 141
     refute @micropost.valid?
   end
+
+  test 'order should be most recent first' do
+    assert_equal Micropost.first, microposts(:most_recent)
+  end
 end
