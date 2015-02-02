58b813f7cd9365c8877c43de5123d5bb59c65f19
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-02-02 17:05:35 -0500

    Micropost UI integration test

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:ariel)
+  end
+
+  test 'micropost interface' do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    # invalid submission
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, micropost: { content: '' }
+    end
+    assert_select 'div#error_explanation'
+    # valid submission
+    content = 'This micropost really ties the room together'
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, micropost: { content: content }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    # delete a post
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # visit a different user
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+end
