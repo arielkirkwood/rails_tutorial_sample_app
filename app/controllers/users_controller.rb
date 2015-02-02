0d30a9393fba9766d08f84621d3d69c6a08d7128
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-02-02 14:54:37 -0500

    UsersController#show: instance var readability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
     @microposts = @user.microposts.paginate(page: params[:page])
+
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
